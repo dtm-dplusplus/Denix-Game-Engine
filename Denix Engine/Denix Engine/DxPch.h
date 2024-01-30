5168eca133c46510c99d6ca6aac153857b00ffd9
dtm-dplusplus <129973785+dtm-dplusplus@users.noreply.github.com>
2024-01-30 09:35:18 +0000

    Pch file

---
+++ Denix Engine/Denix Engine/DxPch.h
@@ -0,0 +1,14 @@
+#pragma once
+
+// C++ Core //
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <string_view>
+
+#include <vector>
+//#include <list>
+//#include <functional>
+
+// Denix Engine
+
---
+++ Denix Engine/Denix Engine/Main.cpp
@@ -0,0 +1,8 @@
+#include "DxPch.h"
+
+int main()
+{
+	printf("Hello World");
+
+	return 0;
+}
