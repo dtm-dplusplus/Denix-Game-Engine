4a7eb1a692dac19fdc1ef7db32581a42cab592ed
dtm-dplusplus <129973785+dtm-dplusplus@users.noreply.github.com>
2024-09-17 15:39:28 +0100

    Content File Path

--- DevProject/src/DevScene.cpp
+++ DevProject/src/DevScene.cpp
@@ -13,42 +13,20 @@ DevScene::DevScene(): Scene("Dev Scene")
 
 void DevScene::Update(float _deltaTime)
 {
-    Scene::Update(_deltaTime);
+	Scene::Update(_deltaTime);
 
-    if(ImGui::Button("Serialize"))
-    {
-		YAML::Emitter Emitter;
-    	Serialize(Emitter);
-    	
-    	// Open file stream
-		std::ofstream fout("DevScene.yaml");
-
-		// Write emitter content to file
-		fout << Emitter.c_str();
-
-		// Close file stream
-		fout.close();
-    	DE_LOG(LogDevProject, Info, "Serialize")
-    	m_SceneObjects.clear();
-    }
-
-	if(ImGui::Button("Deserialize"))
+	if(ImGui::Button("Save Scene"))
 	{
-		YAML::Node n = YAML::LoadFile("DevScene.yaml");
-		//DE_LOG(LogDevProject, Info, n["m_SceneName"].as<std::string>())
-		DE_LOG(LogDevProject, Info, "Deserialize")
+		YAML::Emitter Emitter;
+		Serialize(Emitter);
 	}
 }
 
 bool DevScene::Load()
 {
-    Scene::Load();
-
-
-	// Load node from file
-	YamlObj= MakeRef<Cube>();
-	SpawnSceneObject(YamlObj);
-	return true;
+	YAML::Node sceneNode = YAML::LoadFile("DevScene.yaml");
+	Deserialize(sceneNode);
+	return Scene::Load();
 }
 
 void DevScene::Unload()
@@ -63,23 +41,38 @@ void DevScene::Unload()
 	_out << YAML::Comment("DE_ASSET: Scene");
 	_out << YAML::Newline << YAML::Comment( m_SceneName + " Scene Data");
 	_out << YAML::BeginMap;
-	_out << YAML::Key << "m_SceneName" << YAML::Value << m_SceneName;
-	_out << YAML::Key << "m_Gravity" << YAML::Value << m_Gravity;
-	_out << YAML::BeginSeq;
+	_out << YAML::Key << "m_SceneObjects" << YAML::BeginSeq;
 
-	_out << YAML::Comment("Scene Objects");
-	for (const auto& gameObject : m_SceneObjects)
+	for(auto& gameObject : m_SceneObjects)
 	{
 		_out << YAML::BeginMap;
 		gameObject->Serialize(_out);
 		_out << YAML::EndMap;
 	}
 
-	_out << YAML::EndSeq;
 	_out << YAML::EndMap;
+
+	// Open file stream
+	std::ofstream fout("DevScene.yaml");
+
+	//// Write emitter content to file
+	fout << _out.c_str();
+
+	//// Close file stream
+	fout.close();
+	DE_LOG(LogDevProject, Info, "Serialize")
  }
 
  void DevScene::Deserialize(YAML::Node& _in)
  {
-	
+	DE_LOG(LogDevProject, Info, "Deserialize")
+	YAML::Node sequenceNode = _in["m_SceneObjects"];
+
+	m_SceneObjects.clear();
+	for (YAML::const_iterator it = sequenceNode.begin(); it != sequenceNode.end(); ++it)
+	{
+		Ref<GameObject> gameObj = MakeRef<GameObject>();
+		gameObj->Deserialize(*it);
+		m_SceneObjects.push_back(gameObj);
+	}
  }
