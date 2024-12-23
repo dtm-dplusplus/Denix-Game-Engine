4ba4fff51cd0ca0da9b7c031f9ac3bacdafea090
dtm-dplusplus <129973785+dtm-dplusplus@users.noreply.github.com>
2024-12-23 18:29:37 +0000

    Update RendererSubsystem.cpp

--- DenixEngine/src/Denix/Video/Renderer/RendererSubsystem.cpp
+++ DenixEngine/src/Denix/Video/Renderer/RendererSubsystem.cpp
@@ -55,10 +55,11 @@ namespace Denix
                            GL_FALSE, glm::value_ptr(
                                s_RendererSubSystem->m_ActiveScene->m_ActiveCamera->m_View));
 
-        glUniform3f(m_DefaultShader->GetUniform("u_CameraPosition"), 
+        /*glUniform3f(m_DefaultShader->GetUniform("u_CameraPosition"), 
             s_RendererSubSystem->m_ActiveScene->m_ActiveCamera->m_TransformComponent->m_Position.x,
             s_RendererSubSystem->m_ActiveScene->m_ActiveCamera->m_TransformComponent->m_Position.y,
             s_RendererSubSystem->m_ActiveScene->m_ActiveCamera->m_TransformComponent->m_Position.z);
+            */
 
         
         for (const Ref<Actor>& actor : s_RendererSubSystem->m_ActiveScene->m_Actors)
