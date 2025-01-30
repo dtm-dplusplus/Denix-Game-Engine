88a0d6d5caed9ca1d5ebc1eeefa1ac38ff8df501
Denis Pickering <pickeringdenis02@gmail.com>
2025-01-30 00:47:54 +0000

    Update Object.h

--- DenixEngine/src/Denix/Core/Object.h
+++ DenixEngine/src/Denix/Core/Object.h
@@ -1,91 +1,130 @@
+/**
+ * @file Object.h
+ * @brief Defines the base Object class and ObjectInit struct for game objects.
+ */
+
 #pragma once
 
 #include <string>
-
 #include "Denix/Core.h"
 
 namespace Denix
 {
-    /*
-     * Initialiser for all objects in the game
-     * Only implents the name for now, it will be expanded in the future
-    */
+    /**
+     * @struct ObjectInit
+     * @brief Initializer for all objects in the game.
+     * 
+     * Currently only implements the name but will be expanded in the future.
+     */
     struct ObjectInit
     {
-        // Constructors
-        ObjectInit(): Name{"Object"}
-        {
-        }
+        /**
+         * @brief Default constructor initializing the object name to "Object".
+         */
+        ObjectInit(): Name{"Object"} {}
 
-        ObjectInit(std::string _name) : Name{std::move(_name)}
-        {
-        }
+        /**
+         * @brief Constructor initializing the object with a specified name.
+         * @param _name Name of the object.
+         */
+        ObjectInit(std::string _name) : Name{std::move(_name)} {}
 
+        /**
+         * @brief Name of the object.
+         */
         std::string Name;
     };
 
-    // Base class for all objects in the game
+    /**
+     * @class Object
+     * @brief Base class for all objects in the game.
+     */
     class Object
     {
     public:
-        Object(): m_GUID{GenID()}, m_Name{ObjectInit().Name}, m_IsRubbish{false}
-        {
-        }
+        /**
+         * @brief Default constructor initializing the object with a generated ID and default name.
+         */
+        Object(): m_GUID{GenID()}, m_Name{ObjectInit().Name}, m_IsRubbish{false} {}
 
-        Object(const ObjectInit& _object_init): m_GUID{GenID()}, m_Name{_object_init.Name}, m_IsRubbish{false}
-        {
-        }
+        /**
+         * @brief Constructor initializing the object with a specified initializer.
+         * @param _object_init Object initialization parameters.
+         */
+        Object(const ObjectInit& _object_init): m_GUID{GenID()}, m_Name{_object_init.Name}, m_IsRubbish{false} {}
 
-        // Destructors
+        /**
+         * @brief Virtual destructor.
+         */
         virtual ~Object() = default;
 
         /**
-         * 
-         * @return The name of the object
+         * @brief Gets the name of the object.
+         * @return The name of the object.
          */
         std::string GetName() const { return m_Name; }
 
+        /**
+         * @brief Gets the class name of the object.
+         * @return The class name of the object.
+         */
         std::string GetClassNameDE() const { return m_ClassName; }
 
         /**
-         * Global Unique Identifier 
-         * @return The GUID of the object
+         * @brief Gets the Global Unique Identifier (GUID) of the object.
+         * @return The GUID of the object.
          */
         size_t GetGUID() const { return m_GUID; }
 
+        /**
+         * @brief Checks if the object is marked as rubbish.
+         * @return True if the object is marked for deletion, otherwise false.
+         */
         bool IsRubbish() const { return m_IsRubbish; }
 
     protected:
         /**
-         * Set the name of the object.
-         * Use with caution, the name should be validated before setting
-         * @param _name The name of the object 
+         * @brief Sets the name of the object.
+         * 
+         * Use with caution; the name should be validated before setting.
+         * @param _name The new name of the object.
          */
         void SetName(const std::string& _name) { m_Name = _name; }
 
-
         /**
-         * Mark the object as rubbish, it will be deleted at the end of the frame
-         * Use with caution, the object should be validated before marking.
+         * @brief Marks the object as rubbish. It will be deleted at the end of the frame.
+         * 
+         * Use with caution; the object should be validated before marking.
          */
         void MarkRubbish() { m_IsRubbish = true; }
 
-        /* Class name of the object */
+        /**
+         * @brief Class name of the object.
+         */
         std::string m_ClassName;
 
     private:
-        /* Name and ID should be hashed in some way in the future */
-        /* Unique Identifier */
+        /**
+         * @brief Unique Identifier.
+         * 
+         * Name and ID should be hashed in some way in the future.
+         */
         size_t m_GUID;
 
-        /* Name of the object */
+        /**
+         * @brief Name of the object.
+         */
         std::string m_Name;
 
-
-        /** Clean up value, if marked as rubbish, it will be deleted once the frame has finished*/
+        /**
+         * @brief Clean-up flag. If marked as rubbish, it will be deleted once the frame has finished.
+         */
         bool m_IsRubbish;
 
-        /* Used to assign object IDs. Represents the tota number of objects created */
+        /**
+         * @brief Generates a unique ID for each object.
+         * @return A unique identifier.
+         */
         static unsigned int GenID();
 
         friend class Engine;
