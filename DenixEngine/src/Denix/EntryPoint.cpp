f6945ca4c48ad052bcd803799673d01102fd7171
dtm-dplusplus <129973785+dtm-dplusplus@users.noreply.github.com>
2025-01-30 12:36:41 +0000

    Entry point

--- DenixEngine/src/Denix/EntryPoint.cpp
+++ DenixEngine/src/Denix/EntryPoint.cpp
@@ -1,5 +1,14 @@
 #include "Engine.h"
 
+/**
+ * @brief The main entry point for the Denix Engine application.
+ *
+ * This function initializes the engine and starts its execution.
+ *
+ * @param argc The number of command-line arguments.
+ * @param argv The array of command-line arguments.
+ * @return int Returns 0 upon successful execution.
+ */
 int main(int argc, char** argv)
 {
     const auto engine = Denix::MakeEngine();
